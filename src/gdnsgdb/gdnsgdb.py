ed6da4c3375dc6c77f0dc43705e0e904573931ea
J. "Dorian Greyson" L. <greysondn@gmail.com>
2026-02-06 11:12:35 -0500

    fix indentation and propose palette loading from theoretical config

--- src/gdnsgdb/gdnsgdb.py
+++ src/gdnsgdb/gdnsgdb.py
@@ -22,52 +22,73 @@ class GDNSGDBColor:
         self.green:int = green
         self.blue:int = blue
         
-        @classmethod
-        def from_html(cls, html_color:str) -> GDNSGDBColor:
-            ret:GDNSGDBColor = GDNSGDBColor()
-            
-            # there are probably better algorithms, "but"
-            col_src:str = html_color.lstrip("#")
-           
-            col_src_red:str   = col_src[0:2]
-            col_src_green:str = col_src[2:4]
-            col_src_blue:str  = col_src[4:6]
-            
-            ret.red   = int(col_src_red,   16)
-            ret.green = int(col_src_green, 16)
-            ret.blue  = int(col_src_blue,  16)
-            
-            return ret
+    @classmethod
+    def from_html(cls, html_color:str) -> "GDNSGDBColor":
+        ret:GDNSGDBColor = GDNSGDBColor()
         
-        @classmethod
-        def from_tuple(cls, pil_color:tuple[int, ...]) -> GDNSGDBColor:
-            ret:GDNSGDBColor = GDNSGDBColor()
-            
-            # this one should be straightforward
-            ret.red   = pil_color[0]
-            ret.green = pil_color[1]
-            ret.blue  = pil_color[2]
-            
-            return ret
+        # there are probably better algorithms, "but"
+        col_src:str = html_color.lstrip("#")
         
-        def to_html(self) -> str:
-            return f"#{self.red:02X}{self.green:02X}{self.blue:02X}"
+        col_src_red:str   = col_src[0:2]
+        col_src_green:str = col_src[2:4]
+        col_src_blue:str  = col_src[4:6]
         
-        def to_tuple(self) -> tuple[int, int, int]:
-            return (self.red, self.green, self.blue)
+        ret.red   = int(col_src_red,   16)
+        ret.green = int(col_src_green, 16)
+        ret.blue  = int(col_src_blue,  16)
+        
+        return ret
+    
+    @classmethod
+    def from_tuple(cls, pil_color:tuple[int, ...]) -> "GDNSGDBColor":
+        ret:GDNSGDBColor = GDNSGDBColor()
+        
+        # this one should be straightforward
+        ret.red   = pil_color[0]
+        ret.green = pil_color[1]
+        ret.blue  = pil_color[2]
+        
+        return ret
+    
+    def to_html(self) -> str:
+        return f"#{self.red:02X}{self.green:02X}{self.blue:02X}"
+    
+    def to_tuple(self) -> tuple[int, int, int]:
+        return (self.red, self.green, self.blue)
         
 class GDNSGDB:
     def __init__(self):
-        self.conf:Any = None 
+        self.conf:Any = None
+        """
+        Config for this instance
+        """
     
+        self.palettes:dict[str, dict[int, GDNSGDBColor]]
+        """Palette cache.
+        """
     def load_yaml(self, yaml:str) -> None:
-        """decodes given yaml src data into self.conf
+        """
+        decodes given yaml src data into self.conf and sets related fields.
 
         Args:
             yaml: a yaml src str of some sort
         """
+        # just the barebones load part
         loader:YAML = YAML(typ="safe")
         self.conf = loader.load(yaml)
+        
+        # load the palettes in
+        for flat_color, palette_entries in self.conf["colors"]:
+            # we can get our key for storage and start a value dict
+            swp_k:str = flat_color
+            swp_v:dict[int, GDNSGDBColor] = {}
+            
+            # and then the other part
+            for index, palette_color in palette_entries:
+                swp_v[index] = GDNSGDBColor.from_html(palette_color)
+            
+            # throw it in our dict
+            self.palettes[swp_k] = swp_v
     
     def run(self):
         pass
