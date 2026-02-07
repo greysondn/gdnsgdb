b1b3b58b286719baba255062bac445a05322ce15
J. "Dorian Greyson" L. <greysondn@gmail.com>
2026-02-07 06:51:41 -0500

    Growing convinced there are no good python code formatters

--- src/gdnsgdb/gdnsgdb.py
+++ src/gdnsgdb/gdnsgdb.py
@@ -13,7 +13,7 @@ from typing import (
 
 class GDNSGDBColor:
     """Just a simple color class to convert between RGB tuples and html strings"""
-    def __init__(self, red: int = 0, green: int = 0, blue: int = 0):
+    def __init__(self, red:int = 0, green:int = 0, blue:int = 0):
         """Init.
         <br><br>
         WARNING: This does not validate its inputs. You will see errors down
@@ -24,20 +24,20 @@ class GDNSGDBColor:
             green: channel value 0-255. Defaults to 0.
             blue: channel value 0-255. Defaults to 0.
         """
-        self.red: int = red
-        self.green: int = green
-        self.blue: int = blue
+        self.red:int = red
+        self.green:int = green
+        self.blue:int = blue
     
     @classmethod
-    def from_html(cls, html_color: str) -> "GDNSGDBColor":
-        ret: GDNSGDBColor = GDNSGDBColor()
+    def from_html(cls, html_color:str) -> "GDNSGDBColor":
+        ret:GDNSGDBColor = GDNSGDBColor()
         
         # there are probably better algorithms, "but"
-        col_src: str = html_color.lstrip("#")
+        col_src:str = html_color.lstrip("#")
         
-        col_src_red: str = col_src[0:2]
-        col_src_green: str = col_src[2:4]
-        col_src_blue: str = col_src[4:6]
+        col_src_red:str = col_src[0:2]
+        col_src_green:str = col_src[2:4]
+        col_src_blue:str = col_src[4:6]
         
         ret.red = int(col_src_red, 16)
         ret.green = int(col_src_green, 16)
@@ -46,7 +46,7 @@ class GDNSGDBColor:
         return ret
     
     @classmethod
-    def from_tuple(cls, pil_color: tuple[int, ...]) -> "GDNSGDBColor":
+    def from_tuple(cls, pil_color:tuple[int, ...]) -> "GDNSGDBColor":
         ret: GDNSGDBColor = GDNSGDBColor()
         
         # this one should be straightforward
@@ -67,39 +67,39 @@ class GDNSGDBColor:
 class GDNSGDBMetaTemplate:
     def __init__(
         self,
-        path: Path,
-        color_map: dict[str, int],
-        accept_logo: bool,
-        logo_region: tuple[int, int, int, int] = (0, 0, 0, 0),
-        logo_align_horizontal: str = "center",
-        logo_align_vertical: str = "center"
+        path:Path,
+        color_map:dict[str, int],
+        accept_logo:bool,
+        logo_region:tuple[int, int, int, int] = (0, 0, 0, 0),
+        logo_align_horizontal:str = "center",
+        logo_align_vertical:str = "center"
     ):
-        self.image: Image.Image = Image.open(path)
+        self.image:Image.Image = Image.open(path)
         
-        self.color_map: dict[tuple[int, int, int], int] = {}
+        self.color_map:dict[tuple[int, int, int], int] = {}
         for k, v in color_map.items():
             self.color_map[GDNSGDBColor.from_html(k).to_tuple()] = v
         
-        self.accept_logo: bool = accept_logo
-        self.logo_region: tuple[int, int, int, int] = logo_region
-        self.logo_align_vertical: str = logo_align_vertical
-        self.logo_align_horizontal: str = logo_align_horizontal
+        self.accept_logo:bool = accept_logo
+        self.logo_region:tuple[int, int, int, int] = logo_region
+        self.logo_align_vertical:str = logo_align_vertical
+        self.logo_align_horizontal:str = logo_align_horizontal
 
 
 
 class GDNSGDB:
     def __init__(self):
-        self.conf: Any = None
+        self.conf:Any = None
         """
         Config for this instance
         """
         
-        self.palettes: dict[str, dict[int, GDNSGDBColor]] = {}
+        self.palettes:dict[str, dict[int, GDNSGDBColor]] = {}
         """
         Palette cache.
         """
         
-        self.meta_templates: dict[str, GDNSGDBMetaTemplate] = {}
+        self.meta_templates:dict[str, GDNSGDBMetaTemplate] = {}
         """
         Meta template cache
         """
@@ -113,8 +113,8 @@ class GDNSGDB:
         # load the palettes in
         for flat_color, palette_entries in self.conf["colors"].items():
             # we can get our key for storage and start a value dict
-            swp_k: str = flat_color
-            swp_v: dict[int, GDNSGDBColor] = {}
+            swp_k:str = flat_color
+            swp_v:dict[int, GDNSGDBColor] = {}
             
             # and then the other part
             for index, palette_color in palette_entries.items():
@@ -128,19 +128,19 @@ class GDNSGDB:
         Internal helper solely responsible for loading and caching meta
         templates initially.
         """
-        swp_root: Path = Path(self.conf["meta"]["root_dir"])
+        swp_root:Path = Path(self.conf["meta"]["root_dir"])
         
         for name, data in self.conf.items():
             # the name becomes a key later
-            swp_k: str = name
+            swp_k:str = name
             
             # our v is a bit more complicated, we have to gather the parts
             
             # path
-            swp_path: Path = swp_root / data["path"]
+            swp_path:Path = swp_root / data["path"]
             
             # region
-            swp_region: tuple[int, int, int, int] = (0, 0, 0, 0)
+            swp_region:tuple[int, int, int, int] = (0, 0, 0, 0)
             
             if (data["logo"]["accept"]):
                 swp_region = (
@@ -151,19 +151,19 @@ class GDNSGDB:
                 )
             
             # horizontal align
-            swp_horizontal: str = "center"
+            swp_horizontal:str = "center"
             
             if (data["logo"]["accept"]):
                 swp_horizontal = data["logo"]["align"]["horizontal"]
             
             # vertical align
-            swp_vertical: str = "center"
+            swp_vertical:str = "center"
             
             if (data["logo"]["accept"]):
                 swp_vertical = data["logo"]["align"]["vertical"]
             
             # finally get value
-            swp_v: GDNSGDBMetaTemplate = GDNSGDBMetaTemplate(
+            swp_v:GDNSGDBMetaTemplate = GDNSGDBMetaTemplate(
                 swp_path,
                 data["color_map"],
                 data["logo"]["accept"],
@@ -175,7 +175,7 @@ class GDNSGDB:
             # assign
             self.meta_templates[swp_k] = swp_v
     
-    def load_yaml(self, yaml: str) -> None:
+    def load_yaml(self, yaml:str) -> None:
         """
         decodes given yaml src data into self.conf and sets related fields.
 
@@ -183,7 +183,7 @@ class GDNSGDB:
             yaml: a yaml src str of some sort
         """
         # just the barebones load part
-        loader: YAML = YAML(typ="safe")
+        loader:YAML = YAML(typ="safe")
         self.conf = loader.load(yaml)
         
         # call internal helpers now that conf is set
